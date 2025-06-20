afb237ddda1fa01d1eac753c65e719e5321b9127
alexisis111 <shuragilyow1@gmail.com>
2025-06-20 17:52:41 +0300

    Initial commit from Create Next App

---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-gray-100 to-gray-300 p-4">
+        <h1 className="text-4xl font-bold text-blue-600 mb-4">Привет от Next.js + Tailwind!</h1>
+        <p className="text-lg text-gray-700">Это простая приветственная страница.</p>
+      </main>
+  );
+}
